593fe35e4969ed5fdb348ec217080e5fa86be9bd
Gemma <gemma@gemmaelsom.com>
2021-04-07 17:18:54 +1000

    Began on program scripting and worked on script.txt database

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Ruby-script-app
 T1A3 
+
+# Provide instructions to user as to how to import their script into the file as a .txt
--- main.rb
+++ main.rb
@@ -1,9 +1,28 @@
 require_relative './script_import'
 require_relative './script_search'
+require_relative './script_reader'
 
-if __FILE__ == $0
-    puts "Enter the relative path to your file:"
-    users_file = gets.chomp
-    fileContents = ScriptImport.new(users_file).read_file
-    puts fileContents
+# Greet user
+puts "Welcome to the Interactive Script Reader - Every Actors New Best Friend. I'm Cheeky, and I'm going to help you memorise your lines faster than ever before. Are you ready?"
+users_response = gets.chomp.to_s
+if users_response != 'yes'
+  puts "Did you mean to say 'Yes'?"
+else
+  puts 'Awesome!'
 end
+
+# Script selection
+
+# StarWars Scripts
+if __FILE__ == $0
+puts 'Which Star Wars script would you like to rehearse? StarWars_EpisodeIV.txt? StarWars_EpisodeV.txt? or StarWars_EpisodeVI.txt?'
+    users_file = gets.chomp
+    file_contents = ScriptImport.new(users_file).read_file
+end
+
+# Character selection
+def character_selection
+    puts 'Thank you. Which character would you like to play?'
+    character = gets.chomp
+    puts "Your character is #{character}"
+    end
