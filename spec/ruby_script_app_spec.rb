a0e3747c562a8ad7dc7d572beaf0f645539744ab
Gemma <gemma@gemmaelsom.com>
2021-04-11 15:02:31 +1000

    updated tests

--- spec/ruby_script_app_spec.rb
+++ spec/ruby_script_app_spec.rb
@@ -1,16 +1,35 @@
 require_relative '../script_import'
 require_relative '../script_reader'
 require_relative '../script_search'
+require_relative '../main'
 require 'find'
 require 'tty-font'
 require 'pastel'
 require 'colorize'
 
-describe print_welcome do
-def print_welcome
-print_welcome = it 'should print welcome banner in color'
-font = TTY::Font.new(:doom)
-pastel = Pastel.new
-puts pastel.white(font.write('WELCOME!', letter_spacing: 2))
+describe ScriptSearch do
+    def load_scripts
+    files = []
+    Find.find('./scripts') do |path|
+      files.push(path.delete_prefix('./scripts/')) if path =~ /.*\.txt$/
+    end
+    files
+  end
 end
-end
+
+describe CharacterSearch do
+ def is_character_in_script(script, character)
+        character_is_not_in_script = false
+        script.each do |line|
+          begin
+            if character.downcase == line[0].downcase
+              character_is_not_in_script = true
+              break
+            end
+          rescue
+            next
+          end
+        end
+        character_is_not_in_script
+      end
+    end
